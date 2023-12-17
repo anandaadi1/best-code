41bf443990aa074536a873ca3f9e25fab384ee0a
Aditya Kumar <152949267+anandaadi1@users.noreply.github.com>
2023-12-17 12:33:46 +0530

    google-translate added

---
+++ google-translate/README.md
@@ -0,0 +1,3 @@
+# Google Translate
+
+`main.js` contains a function that can translate a sentence
--- google-translate/main.js
+++ google-translate/main.js
@@ -1,4 +1,7 @@
-const t = async (text) => {
+// This function can translate english text to hindi
+// no key required
+
+const translate = async (text) => {
    // this function only works for english to hindi
    if (text.trim() === "") return; // if no value provided
 
