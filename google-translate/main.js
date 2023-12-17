0f4b25a11cbf9fa631287a5b87d2fa023f8e9e7e
Aditya Kumar Anand <152949267+anandaadi1@users.noreply.github.com>
2023-12-17 12:13:24 +0530

    Create main.js

---
+++ google-translate/main.js
@@ -0,0 +1,11 @@
+const t = async (text) => {
+   // this function only works for english to hindi
+   if (text.trim() === "") return; // if no value provided
+
+   const url =
+      "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=hi&dt=t&q=" +
+      encodeURIComponent(text);
+   const response = await fetch(url);
+   const data = await response.json();
+   return data[0][0][0];
+};
